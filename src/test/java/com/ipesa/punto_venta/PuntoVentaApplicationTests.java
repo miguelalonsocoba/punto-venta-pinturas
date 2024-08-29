873fdebeb535ab5966d752eb9f64d8db52032986
miguelalonsocoba <miguetheking53@gmail.com>
2024-08-28 19:17:56 -0600

    Inicialización del proyecto

---
+++ src/test/java/com/ipesa/punto_venta/PuntoVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipesa.punto_venta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
